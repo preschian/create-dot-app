4d468627080ce4a6b4ff166b37e704b9e274c845
Preschian Febryantara <preschian@users.noreply.github.com>
2025-06-08 20:31:35 +0700

    chore: eslint at root (#1)

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -6,14 +6,14 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/eslint',
     '@nuxt/fonts',
-    '@nuxt/icon'
+    '@nuxt/icon',
   ],
 
   css: ['~/assets/css/main.css'],
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-11-27'
+  compatibilityDate: '2024-11-27',
 })
