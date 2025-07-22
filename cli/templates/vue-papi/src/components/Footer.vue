0ee84507773df33e71165d2795b983656d8a4b24
Preschian Febryantara <preschian@users.noreply.github.com>
2025-07-22 19:38:53 +0700

    fix: update copyright notice in Footer component

--- cli/templates/vue-papi/src/components/Footer.vue
+++ cli/templates/vue-papi/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer footer-center bg-base-200 text-base-content p-4">
     <aside class="flex items-center gap-4">
-      <p>Copyright © 2025. All rights reserved.</p>
+      <p>Copyright © 2025</p>
       <a href="https://x.com/" target="_blank" class="size-5 text-gray-600 hover:text-gray-900 transition-colors">
         <span class="icon-[mdi--twitter] size-5" />
       </a>
