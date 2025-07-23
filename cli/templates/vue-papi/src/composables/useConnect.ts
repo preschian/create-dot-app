d6589b2bf76e80bebe9daa1ba4d5448cd111c760
Preschian Febryantara <preschian@users.noreply.github.com>
2025-07-23 15:31:24 +0700

    feat: implement local storage management for wallet and account selection in useConnect composable

--- cli/templates/vue-papi/src/composables/useConnect.ts
+++ cli/templates/vue-papi/src/composables/useConnect.ts
@@ -3,8 +3,24 @@ import { getWallets } from '@talismn/connect-wallets'
 import { connectInjectedExtension } from 'polkadot-api/pjs-signer'
 import { ref } from 'vue'
 
-const selectedAccount = ref<WalletAccount | null>(null)
-const connectedWallet = ref<Wallet | null>(null)
+const storageWallet = 'dapp:wallet'
+const storageAccount = 'dapp:account'
+
+function getStorage(key: string) {
+  const value = localStorage.getItem(key)
+  return value ? JSON.parse(value) : null
+}
+
+function setStorage(key: string, value: any) {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+function removeStorage(key: string) {
+  localStorage.removeItem(key)
+}
+
+const selectedAccount = ref<WalletAccount | null>(getStorage(storageAccount))
+const connectedWallet = ref<Wallet | null>(getStorage(storageWallet))
 const listAccounts = ref<WalletAccount[]>([])
 const isConnecting = ref<string | null>(null)
 
@@ -17,7 +33,10 @@ export function useConnect() {
     try {
       isConnecting.value = wallet.extensionName
       listAccounts.value = []
+
+      // set connected wallet
       connectedWallet.value = wallet
+      setStorage(storageWallet, wallet)
 
       await wallet.enable('CDA')
       const accounts = await wallet.getAccounts()
@@ -32,6 +51,7 @@ export function useConnect() {
       console.error(err)
       isConnecting.value = null
       connectedWallet.value = null
+      removeStorage(storageWallet)
     }
   }
 
@@ -39,10 +59,13 @@ export function useConnect() {
     selectedAccount.value = null
     connectedWallet.value = null
     listAccounts.value = []
+    removeStorage(storageAccount)
+    removeStorage(storageWallet)
   }
 
   function selectAccount(account: WalletAccount) {
     selectedAccount.value = account
+    setStorage(storageAccount, account)
   }
 
   return {
