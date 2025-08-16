a139774a88d48dbdfe2683d2b6b2c44e9aca1e57
roiLeo <medina.leo42@gmail.com>
2025-08-16 14:14:16 +0200

    feat: useCurrentBlock client-side

--- templates/nuxt-papi/app/composables/useCurrentBlock.ts
+++ templates/nuxt-papi/app/composables/useCurrentBlock.ts
@@ -1,4 +1,9 @@
 export function useCurrentBlock(chain: Prefix) {
+  // make sure to run this composable only on client side
+  if (import.meta.server) {
+    return {}
+  }
+
   const name = ref('')
   const currentBlock = ref(0)
   const isConnected = computed(() => currentBlock.value > 0)
