6d617a6c9ef24cdda375b3622904fea1bab2fef5
Preschian Febryantara <preschian@users.noreply.github.com>
2025-09-02 10:16:59 +0700

    fix: next-papi missing dep

--- templates/next-papi/app/page.tsx
+++ templates/next-papi/app/page.tsx
@@ -9,6 +9,7 @@ import { chainKeys } from './utils/sdk'
 
 export default function Home() {
   const { selectedAccount } = useConnect()
+  console.log('trigger build')
 
   return (
     <div className="min-h-screen flex flex-col justify-between">
