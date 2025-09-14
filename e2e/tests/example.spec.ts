0af8b2e0d5624e8e6f6ce2f4c6fd7a5e13806d78
Preschian Febryantara <preschian@users.noreply.github.com>
2025-09-14 23:55:52 +0700

    feat: enhance example test to support multiple Polkadot DApp URLs and add UI testing script

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,18 +1,34 @@
 import { test } from '@avalix/chroma';
 
-const POLKADOT_DAPP_URL = 'https://polkadot-starter-next-dedot.vercel.app/'
+// test.describe.configure({ mode: 'serial' });
+
+const POLKADOT_DAPP_URLS = [
+  'https://polkadot-starter-next-dedot.vercel.app/',
+  'https://polkadot-starter-next-papi.vercel.app/',
+  'https://polkadot-starter-react-dedot.vercel.app/',
+  'https://polkadot-starter-react-papi.vercel.app/',
+
+  'https://polkadot-starter-nuxt-dedot.vercel.app/',
+  'https://polkadot-starter-nuxt-papi.vercel.app/',
+  'https://polkadot-starter-vue-dedot.vercel.app/',
+  'https://polkadot-starter-vue-papi.vercel.app/',
+]
+
 const ACCOUNT_NAME = '// Alice'
 const DOT_TEST_MNEMONIC = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk'
 const DOT_TEST_PASSWORD = 'secure123!'
 
-test('sign transaction', async ({ page, importAccount, authorize, approveTx }) => {
+POLKADOT_DAPP_URLS.forEach((url) => {
+  test(`sign transaction on ${url}`, async ({ page, importAccount, authorize, approveTx }) => {
+  console.log(`🧪 Testing ${url}`)
+  
   await importAccount({
     seed: DOT_TEST_MNEMONIC,
     password: DOT_TEST_PASSWORD,
     name: ACCOUNT_NAME,
   })
 
-  await page.goto(POLKADOT_DAPP_URL)
+  await page.goto(url)
   await page.waitForLoadState('networkidle')
 
   await page.getByRole('button', { name: /Connect Wallet/i }).click()
@@ -30,11 +46,13 @@ test('sign transaction', async ({ page, importAccount, authorize, approveTx }) =
   await page.getByText(ACCOUNT_NAME).click()
 
   await page.getByRole('button', { name: 'Sign Transaction' }).nth(3).click()
-
+  
+  if (url.includes('papi')) await page.waitForTimeout(3000)
   await approveTx({ password: DOT_TEST_PASSWORD })
   await page.getByText('Processing transaction...').waitFor({ state: 'visible' })
 
-  console.log('🎉 Test completed successfully!')
+  console.log(`🎉 Test completed successfully for ${url}!`)
 
   await page.waitForTimeout(3000)
-});
+  })
+})
