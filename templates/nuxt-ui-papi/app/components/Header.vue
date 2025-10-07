e658fb01e1913ceeff13a3967ac8b2c4662e21d4
roiLeo <medina.leo42@gmail.com>
2025-10-07 14:17:02 +0200

    ✨ nuxt ui templates

---
+++ templates/nuxt-ui-papi/app/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <UHeader :toggle="false">
+    <template #left>
+      <a href="https://polkadot.com" target="_blank" class="flex items-center">
+        <span class="icon-[token-branded--polkadot] text-2xl" />
+        <span class="text-xl font-bold font-mono text-black tracking-wide ml-1">
+          Dapp
+        </span>
+      </a>
+    </template>
+
+    <template #right>
+      <Connect />
+    </template>
+  </UHeader>
+</template>
---
+++ templates/nuxt-ui-papi/app/plugins/sdk.client.ts
@@ -0,0 +1,51 @@
+import type { PolkadotClient, TypedApi } from 'polkadot-api'
+import { createClient } from 'polkadot-api'
+import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat'
+import { getWsProvider } from 'polkadot-api/ws-provider'
+import { ref } from 'vue'
+import { dot, dot_asset_hub, pas, pas_asset_hub } from '~/descriptors'
+
+const config = {
+  dot: {
+    descriptor: dot,
+    providers: ['wss://dot-rpc.stakeworld.io'],
+  },
+  dot_asset_hub: {
+    descriptor: dot_asset_hub,
+    providers: ['wss://dot-rpc.stakeworld.io/assethub'],
+  },
+  pas: {
+    descriptor: pas,
+    providers: ['wss://pas-rpc.stakeworld.io'],
+  },
+  pas_asset_hub: {
+    descriptor: pas_asset_hub,
+    providers: ['wss://pas-rpc.stakeworld.io/assethub'],
+  },
+} as const
+
+export type Prefix = keyof typeof config
+export const chainKeys = Object.keys(config) as Prefix[]
+
+const clients = ref<Partial<Record<Prefix, PolkadotClient>>>({})
+
+function sdk<T extends Prefix>(chain: T) {
+  if (!clients.value[chain]) {
+    clients.value[chain] = createClient(
+      withPolkadotSdkCompat(
+        getWsProvider(config[chain].providers[0]),
+      ),
+    )
+  }
+
+  return {
+    api: clients.value[chain]!.getTypedApi(config[chain].descriptor) as TypedApi<typeof config[T]['descriptor']>,
+    client: clients.value[chain]!,
+  }
+}
+
+export default defineNuxtPlugin(() => {
+  return {
+    provide: { sdk },
+  }
+})
