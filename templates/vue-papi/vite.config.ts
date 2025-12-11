0120edc36502fecc0b6411ce9bf98709b9897db1
Preschian Febryantara <preschian@users.noreply.github.com>
2025-12-11 12:58:59 +0700

    chore: add Rollup external configuration for React in vue-papi template

--- templates/vue-papi/vite.config.ts
+++ templates/vue-papi/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
 })
