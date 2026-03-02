2a79bb8f2cf75e5b983200167fb4c0f65a20c70e
Preschian Febryantara <preschian@users.noreply.github.com>
2026-03-02 14:06:53 +0700

    feat: update layout and add copyable command component
    - Changed font from Inter to JetBrains Mono in layout.
    - Introduced a new CopyableCommand component for easy command copying.
    - Enhanced HomePage with a new layout, including a hero section and feature cards.
    - Updated GitHub configuration for the project.

--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -1,16 +1,15 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
-// fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'preschian',
+  repo: 'create-dot-app',
   branch: 'main',
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: 'Create Dot App',
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
